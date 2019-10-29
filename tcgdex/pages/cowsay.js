a0242a509740b32b48044526354f1529faed9853
SNilli <mklx1319@gmail.com>
2019-10-29 18:23:32 +0700

    add text area for dynamic cow say

--- tcgdex/pages/cowsay.js
+++ tcgdex/pages/cowsay.js
@@ -1,7 +1,44 @@
+import { useState } from "react";
 import cowsay from "cowsay-browser";
 
+const initState = {
+  text: "Moo Moo!"
+};
+
 function CowsayHi() {
-  return <pre>{cowsay.say({ text: "hi there!" })}</pre>;
+  const [state, setState] = useState(initState);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const text = document.getElementById("cowsay").value;
+    console.log(text);
+    const newState = {
+      text: text
+    };
+    setState(newState);
+  };
+
+  const a = e => {
+    setState({
+      text: e.target.value
+    });
+  };
+
+  const { text } = state;
+  return (
+    <div>
+      <pre>{cowsay.say({ text })}</pre>
+      <label htmlFor="cowsay">Cow say Here!</label>
+      <br />
+      <textarea
+        name="cowsay"
+        id="cowsay"
+        cols="20"
+        rows="5"
+        onChange={a}
+      ></textarea>
+    </div>
+  );
 }
 
 export default CowsayHi;
