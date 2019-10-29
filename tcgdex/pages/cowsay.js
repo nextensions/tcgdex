9a2a4810563fc2c2ed04f7c44cbd9ee864d5aedb
SNilli <mklx1319@gmail.com>
2019-10-29 18:33:12 +0700

    uncommit

--- tcgdex/pages/cowsay.js
+++ tcgdex/pages/cowsay.js
@@ -1,44 +1,7 @@
-import { useState } from "react";
 import cowsay from "cowsay-browser";
 
-const initState = {
-  text: "Moo Moo!"
-};
-
 function CowsayHi() {
-  const [state, setState] = useState(initState);
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    const text = document.getElementById("cowsay").value;
-    console.log(text);
-    const newState = {
-      text: text
-    };
-    setState(newState);
-  };
-
-  const a = e => {
-    setState({
-      text: e.target.value
-    });
-  };
-
-  const { text } = state;
-  return (
-    <div>
-      <pre>{cowsay.say({ text })}</pre>
-      <label htmlFor="cowsay">Cow say Here!</label>
-      <br />
-      <textarea
-        name="cowsay"
-        id="cowsay"
-        cols="20"
-        rows="5"
-        onChange={a}
-      ></textarea>
-    </div>
-  );
+  return <pre>{cowsay.say({ text: "hi there!" })}</pre>;
 }
 
 export default CowsayHi;
