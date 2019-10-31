464a38ac59d8a1d5c100b7097e291118179df903
SNilli <mklx1319@gmail.com>
2019-10-31 17:46:08 +0700

    add cowsay animetion chatbox introduce Team Rocket

--- tcgdex/pages/cowsay.js
+++ tcgdex/pages/cowsay.js
@@ -1,35 +1,58 @@
-import { useState } from "react";
-import cowsay from "cowsay-browser";
+import { useState, useEffect } from "react";
+import { say } from "cowsay-browser";
 import { produce } from "immer";
 
 const initState = {
-  text: "Moo Moo!"
+  text: " "
 };
+
+const a = `Prepare for trouble!
+And make it double!
+To protect the world from devastation!
+To unite all peoples within our nation!
+To denounce the evils of truth and love!
+To extend our reach to the stars above!
+Jessie!
+James!
+Team Rocket blasts off at the speed of light!
+Surrender now, or prepare to fight!`;
 
 function CowsayHi() {
   const [state, setState] = useState(initState);
 
-  const handleTextChange = e => {
-    const newState = produce(state, draftState => {
-      console.log(draftState);
-      draftState["text"] = e.target.value;
-    });
-    setState(newState);
-  };
+  useEffect(() => {
+    const words = a.split(" ");
+    let { text: newText } = state;
+    let currentWord = words[0];
+    setInterval(() => {
+      if (words.length === 0) return;
+      let print = currentWord[0];
+      currentWord = currentWord.substr(1, currentWord.length - 1);
+
+      const newState = produce(state, draftState => {
+        draftState["text"] = (newText += print).trimStart();
+      });
+      setState(newState);
+      if (currentWord.length === 0) {
+        const newState = produce(state, draftState => {
+          draftState["text"] = (newText += " ").trimStart();
+        });
+        setState(newState);
+        words.shift();
+        currentWord = words[0];
+      }
+    }, 75);
+  }, []);
 
   const { text } = state;
   return (
     <div>
-      <pre>{cowsay.say({ text })}</pre>
-      <label htmlFor="cowsay">Cow say Here!</label>
-      <br />
-      <textarea
-        name="cowsay"
-        id="cowsay"
-        cols="20"
-        rows="5"
-        onChange={handleTextChange}
-      ></textarea>
+      <pre>
+        {say({
+          text,
+          f: "elephant"
+        })}
+      </pre>
     </div>
   );
 }
